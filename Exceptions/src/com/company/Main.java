4d76bbf7d86b3adc4f0df141f9a5778b3793c09b
Ana-Maria Melinte <34279579+AnMa12@users.noreply.github.com>
2018-04-17 13:51:55 +0300

    creating my own exceptions

---
+++ Exceptions/src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	/* Clasa Main.java va contine doua obiecte de tipul CofeeMaker si
+       CofeeDrinker si veti genera temperatura si concentratia pentru 5 cafele. In functie
+       de valorile generate, utilizand blocuri try-catch si finally veti afisa mesajele
+       provenite din fiecare exceptie in parte. */
+	CoffeeMaker c1, c2;
+    }
+}
