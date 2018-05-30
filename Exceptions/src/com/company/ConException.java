873e430294c8aa418d5a7bcdaeb4ab71dc0c800d
Ana-Maria Melinte <34279579+AnMa12@users.noreply.github.com>
2018-05-30 19:57:25 +0300

    cleaning the folders

--- Exceptions/src/com/company/ConException.java
+++
@@ -1,7 +0,0 @@
-package com.company;
-
-public class ConException extends Exception {
-    public ConException (String msg) {
-        super(msg);
-    }
-}
--- Exceptions/src/com/company/TempException.java
+++
@@ -1,7 +0,0 @@
-package com.company;
-
-public class TempException extends Exception {
-    public TempException (String msg) {
-        super(msg);
-    }
-}
