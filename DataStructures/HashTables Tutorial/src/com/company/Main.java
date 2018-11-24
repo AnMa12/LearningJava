c561d2cf1350f13ae85b91a19560ab5d20df0e12
Ana-Maria Melinte <34279579+AnMa12@users.noreply.github.com>
2018-11-24 23:16:33 +0200

    learning about Iterator for Collection and Set

--- DataStructures/HashTables Tutorial/src/com/company/Main.java
+++ DataStructures/HashTables Tutorial/src/com/company/Main.java
@@ -1,63 +1,45 @@
 package com.company;
 
-import java.util.Collection;
-import java.util.Enumeration;
-import java.util.Hashtable;
-import java.util.Set;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class Main {
 
     public static void main(String args[]) {
 
-        Hashtable<String, String> myHash = new Hashtable<String, String>();
+        Hashtable<String, String> capitalsHash = new Hashtable<String, String>();
 
-        //1)put an object into Hashtable
-        myHash.put("Romania", "Bucharest");
-        myHash.put("France", "Paris");
-        myHash.put("Spain", "Madrid");
+        capitalsHash.put("Romania", "Bucharest");
+        capitalsHash.put("France", "Paris");
+        capitalsHash.put("Italy", "Rome");
 
-        //2)retrieve an object from Hashtable
-        //retrieve the value using the key
-        System.out.println(myHash.get("Romania"));
+        System.out.println(capitalsHash.get("Romania"));
 
-        //4)check if hashtable contains a particular value
-        System.out.println(myHash.containsValue("Paris"));
-        System.out.println(myHash.containsValue("Berlin"));
+        System.out.println(capitalsHash.containsValue("Paris"));
+        System.out.println(capitalsHash.containsKey("Italy"));
 
-        //5)check if hashtable contains a particular key
-        System.out.println(myHash.containsKey("Sweden"));
-        System.out.println(myHash.containsKey("Romania"));
+        Enumeration enumHashValues = capitalsHash.elements();
+        while(enumHashValues.hasMoreElements()) {
+            System.out.print(enumHashValues.nextElement());
+        }
+        System.out.println();
 
-        //7)check if hashtable is empty
-        System.out.println(myHash.isEmpty());
+        Collection collectionHashValues = capitalsHash.values();
+        Iterator<String> iterator = collectionHashValues.iterator();
+        while(iterator.hasNext())
+            System.out.print(iterator.next() + " ");
+        System.out.println();
 
-        //9)the size of hashtable
-        System.out.println(myHash.size());
+        Enumeration enumHashKeys = capitalsHash.keys();
+        while(enumHashKeys.hasMoreElements()) {
+            System.out.print(enumHashKeys.nextElement());
+        }
+        System.out.println();
 
-        System.out.println("***");
-
-        //10)get all the values
-        Collection hashValues = myHash.values();
-        Enumeration hashEnum = myHash.elements();
-
-        System.out.println(myHash.values());
-        System.out.println(myHash.elements());
-
-        /*while(hashEnum.hasMoreElements())
-            System.out.print(hashEnum.nextElement() + " ");
-        System.out.println();*/
-
-        //11)get all the keys
-        Set hashKeys = myHash.keySet();
-        Enumeration hashEnumKeys = myHash.keys();
-
-        System.out.println(myHash.keySet());
-        System.out.println(myHash.keys());
-
-        System.out.println("***");
-
-        myHash.clear();
-        System.out.println(myHash.size());
+        Set setHashKeys = capitalsHash.keySet();
+        Iterator<String> iteratorKeys = setHashKeys.iterator();
+        while(iteratorKeys.hasNext())
+            System.out.print(iteratorKeys.next() + " ");
+        System.out.println();
     }
 }
