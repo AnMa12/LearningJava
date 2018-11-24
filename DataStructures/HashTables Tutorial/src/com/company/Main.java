343637ad123a25fa755f03cd8219d1477fdcea95
Ana-Maria Melinte <34279579+AnMa12@users.noreply.github.com>
2018-11-24 23:43:41 +0200

    HashTable with linked list

--- DataStructures/HashTables Tutorial/src/com/company/Main.java
+++ DataStructures/HashTables Tutorial/src/com/company/Main.java
@@ -7,7 +7,7 @@ public class Main {
 
     public static void main(String args[]) {
 
-        Hashtable<String, String> capitalsHash = new Hashtable<String, String>();
+        /*Hashtable<String, String> capitalsHash = new Hashtable<String, String>();
 
         capitalsHash.put("Romania", "Bucharest");
         capitalsHash.put("France", "Paris");
@@ -40,6 +40,32 @@ public class Main {
         Iterator<String> iteratorKeys = setHashKeys.iterator();
         while(iteratorKeys.hasNext())
             System.out.print(iteratorKeys.next() + " ");
-        System.out.println();
+        System.out.println();*/
+
+        Hashtable<String,LinkedList<String>> h = new  Hashtable<String, LinkedList<String>>();
+
+        h.put("Romania", new LinkedList<String>());
+        LinkedList<String> cities = h.get("Romania");
+        cities.push("Galati");
+        cities.push("Braila");
+
+        h.put("France", new LinkedList<String>());
+        LinkedList<String> cities2 = h.get("France");
+        cities2.push("Nice");
+
+        h.put("Spain", new LinkedList<String>());
+        LinkedList<String> cities3 = h.get("Spain");
+        cities3.push("Madrid");
+        cities3.push("Barcelona");
+        cities3.push("Sevilla");
+
+
+        //get the hashtable values
+        Enumeration enumHValues = h.elements();
+        while(enumHValues.hasMoreElements()) {
+            LinkedList<String> list = (LinkedList<String>)enumHValues.nextElement();
+            for(String l : list)
+            System.out.println(l);
+        }
     }
 }
