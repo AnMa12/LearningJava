725a52e94ecb889b904c9a3b914969538d731795
Ana-Maria Melinte <34279579+AnMa12@users.noreply.github.com>
2018-12-02 14:51:04 +0200

    graph2

---
+++ DataStructures/Graphs2/src/com/company/Graph.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Graph {
+
+    private HashMap<Integer, Node> nodeLookup = new HashMap<Integer, Node>();
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
