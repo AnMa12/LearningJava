f2136c83e7a4543c9b098a4cb65ee07d9a488032
Ana-Maria Melinte <34279579+AnMa12@users.noreply.github.com>
2018-12-02 14:51:49 +0200

    tree traversals

--- DataStructures/TreeTraversals/src/com/company/Main.java
+++ DataStructures/TreeTraversals/src/com/company/Main.java
@@ -28,6 +28,22 @@ class BinaryTree {
         }
     }
 
+    public static Node addNode(Node root, int data){
+        if(root == null)
+            return new Node(data);
+        else {
+            Node cur;
+            if(data <= root.left.data) {
+                cur = addNode(root.left, data);
+                root.left = cur;
+            } else {
+                cur = addNode(root.right, data);
+                root.right = cur;
+            }
+            return root;
+        }
+    }
+
     public void printBinaryTreeLevel(Node root, int level) {
         if(root == null)
             return;
@@ -50,11 +66,21 @@ class BinaryTree {
     public static void main(String args[]) {
         BinaryTree myBT = new BinaryTree();
 
+        /*
         myBT.root = new Node(1);
         myBT.root.left = new Node(2);
         myBT.root.right = new Node(3);
         myBT.root.left.left = new Node(4);
         myBT.root.left.right = new Node(5);
+        */
+        myBT.root = null;
+        myBT.root = addNode(myBT.root, 5);
+        myBT.root = addNode(myBT.root, 3);
+        myBT.root = addNode(myBT.root, 8);
+        myBT.root = addNode(myBT.root, 10);
+        myBT.root = addNode(myBT.root, 4);
+
+        myBT.getBinaryTreeHeight(myBT.root);
 
         myBT.printByLevelBinaryTree(myBT.root);
 
